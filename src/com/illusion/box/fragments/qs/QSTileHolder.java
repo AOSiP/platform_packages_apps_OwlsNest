55c3ec5c6773518a4c932e0be090e39f3e20a33d
Roman Birg <roman@cyngn.com>
2015-01-23 18:28:03 -0800

    Settings: add newly added toggles
    Change-Id: I338eae4546bbc6c9f8453141bf2a8e5afb41fa7b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	res/values/cm_strings.xml

--- src/com/illusion/box/fragments/qs/QSTileHolder.java
+++ src/com/illusion/box/fragments/qs/QSTileHolder.java
@@ -94,6 +94,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
                 break;
+            case QSConstants.TILE_NFC:
+                resourceName = "ic_qs_nfc_on";
+                stringId = R.string.qs_title_nfc;
+                break;
             default:
                 return null;
         }
