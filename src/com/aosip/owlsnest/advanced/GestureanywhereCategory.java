d30ecce2dc3e8d0be30c886df719efd8b2bcab20
droyd <mikew8978@gmail.com>
2015-12-18 23:16:51 -0500

    Basic android sliding tabs
    This commit is based of https://github.com/astuetz/PagerSlidingTabStrip and has been combined with our Owlsnest app that we used in the past
    Most if not all code credit goes to astuetz with minor edits to the basic scheme by me
    from here on out we will keep all settings in settings not an external apk but still maintain our sliding tabs
    
    I also roughed in my entire custom owlsnest settings with blank empty tabs to help others better understand how it works so they can use it to
    
    how it works:
    
    Starts with the basic preference screen for main headers (owlsnest.xml)
    	*In this file each preference calls a special java(example StatusbarHolder.java) written to impliment its own tabs
    	*StatusbarHolder.java then inflates a layout(tab_ui.xml) that sets up the magic PagerSlidingTabs(special tabs at the top) and viewpager(the actual pages)
    	*To add /remove tabs you will edit the lines
    		**frags[0] = new ClockCategory(); (this needs to be named the same as the java used for the settings page you want to add
    		**getString(R.string.clock_category), (this will give your tab the name
    
    Special thanks to astuetz without his original work this wouldnt be possible on AOSiP
    
    For more detailed instruction on how to impliment this see https://android-arsenal.com/details/1/1100
    
    N bringup by @xlxfoxxlx

---
+++ src/com/aosip/owlsnest/advanced/GestureanywhereCategory.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2016 Android Open Source Illusion Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aosip.owlsnest.advanced;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class GestureanywhereCategory extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.OWLSNEST;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.aosip_gestureanywhere);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
+
--- src/com/aosip/owlsnest/fragments/PlaceHolder.java
+++
@@ -1,4 +0,0 @@
-package com.aosip.owlsnest.fragments;
-public class PlaceHolder {
-  // Does Nothing, nuke when adding back actual fragments
-}
