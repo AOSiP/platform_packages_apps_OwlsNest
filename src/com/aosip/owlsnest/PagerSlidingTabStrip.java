55112e2d0eacea2948b016a2ca33c1e5639f1597
xlxfoxxlx <joshfox87@gmail.com>
2016-06-05 12:41:45 -0400

    Adjust Owlsnest TabUI

--- src/com/aosip/owlsnest/PagerSlidingTabStrip.java
+++ src/com/aosip/owlsnest/PagerSlidingTabStrip.java
@@ -80,8 +80,8 @@ public class PagerSlidingTabStrip extends HorizontalScrollView {
 	private boolean textAllCaps = true;
 
 	private int scrollOffset = 52;
-	private int indicatorHeight = 8;
-	private int underlineHeight = 2;
+	private int indicatorHeight = 4;
+	private int underlineHeight = 1;
 	private int dividerPadding = 12;
 	private int tabPadding = 24;
 	private int dividerWidth = 1;
