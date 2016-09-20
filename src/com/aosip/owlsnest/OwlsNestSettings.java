07caffe621e4de453fab50b5912792f983f14ca8
Akhil Narang <akhil@akhilnarang.me>
2016-09-20 10:33:27 +0530

    @xlxfoxxlx try launching this file
    Change-Id: I4bc566791ee58c5bca75a58ecd9d32da97e38db2
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

---
+++ src/com/aosip/owlsnest/OwlsNestSettings.java
@@ -0,0 +1,35 @@
+package com.aosip.owlsnest;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class OwlsNestSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "OwlsNestSettings";
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.owlsnest);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        return true;
+    }
+}
