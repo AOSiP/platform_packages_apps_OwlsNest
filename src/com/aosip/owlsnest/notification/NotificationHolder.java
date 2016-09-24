ad586454170bef6bd6fffc1ab57620447010bb6f
xplodwild <xplod@ouverta.fr>
2016-09-24 15:17:56 +0200

    LED lights customization [2/3]
    Battery And Notification Light customization (2/3)
    
    @darkobas (Omnirom): code fixes and improvements
    @ezio84: add missing things and fixes, remove Calls and Voicemail custom
    		led now that the calls manager lets the Dialer handle its
    		own notifications
    @xyyx (nitrogen): add missing things and fixes, led Settings UI improvements
    
    Light settings: reduce tone if preview image color is very light
    Prevents material whiteout in color preview.
    
    Change-Id: I2f24b0a157f9aa87298fa8c5bd5d2571afe1ca88

--- src/com/aosip/owlsnest/notification/NotificationHolder.java
+++ src/com/aosip/owlsnest/notification/NotificationHolder.java
@@ -51,7 +51,9 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.aosip.owlsnest.notification.HeadsUpCategory;
+import com.aosip.owlsnest.notification.BatteryLightSettings;
 import com.aosip.owlsnest.notification.NotificationCategory;
+import com.aosip.owlsnest.notification.NotificationLightSettings;
 import com.aosip.owlsnest.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
@@ -107,7 +109,9 @@ public class NotificationHolder extends SettingsPreferenceFragment {
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
             frags[0] = new NotificationCategory();
-            frags[1] = new HeadsUpCategory();
+            frags[1] = new BatteryLightSettings();
+            frags[2] = new HeadsUpCategory();
+            frags[3] = new NotificationLightSettings();
         }
 
         @Override
@@ -130,7 +134,9 @@ public class NotificationHolder extends SettingsPreferenceFragment {
         String titleString[];
         titleString = new String[]{
                     getString(R.string.notification_category),
-                    getString(R.string.heads_up_notifications)};
+                    getString(R.string.battery_light_settings),
+                    getString(R.string.heads_up_notifications),
+                    getString(R.string.notification_light_title)};
         return titleString;
     }
 }
