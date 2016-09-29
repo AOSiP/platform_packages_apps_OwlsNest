aaa33b9d77a49287b3e4947f3ae849e28e8b2b55
xlxfoxxlx <joshfox87@gmail.com>
2016-09-28 23:57:11 -0400

    OwlsNest: Create Notifications Category

---
+++ src/com/aosip/owlsnest/notification/NotificationCategory.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 Android Open Source Illusion Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aosip.owlsnest.notification;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.Gravity;
+import com.android.settings.R;
+import android.provider.Settings.SettingNotFoundException; 
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class NotificationCategory extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+    private static final String TAG = "NotificationCategory";
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.OWLSNEST;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.aosip_notification);
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+      }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {      
+       return false;
+    }
+}
+
