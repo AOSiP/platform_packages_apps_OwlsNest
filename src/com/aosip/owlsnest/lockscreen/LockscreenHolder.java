32e3bcef22a883207ea0a75ceca518b1628324db
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-10-09 18:46:16 +0800

    Lockscreen Weather squash: Lock screen: Weather panel improvements Ls weather: options to hide the weather when notifications are visible
    Change-Id: I9de2a3614e2248ae7096c75942d64998264d4040

--- src/com/aosip/owlsnest/lockscreen/LockscreenHolder.java
+++ src/com/aosip/owlsnest/lockscreen/LockscreenHolder.java
@@ -32,6 +32,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.aosip.owlsnest.lockscreen.OptionsCategory;
+import com.aosip.owlsnest.lockscreen.WeatherCategory;
 import com.aosip.owlsnest.PagerSlidingTabStrip;
 
 public class LockscreenHolder extends SettingsPreferenceFragment {
@@ -84,6 +85,7 @@ public class LockscreenHolder extends SettingsPreferenceFragment {
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
             frags[0] = new OptionsCategory();
+            frags[1] = new WeatherCategory();
         }
 
         @Override
@@ -105,7 +107,8 @@ public class LockscreenHolder extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-                    getString(R.string.options_category)};
+                    getString(R.string.options_category),
+                    getString(R.string.weather_category)};
         return titleString;
     }
 }
