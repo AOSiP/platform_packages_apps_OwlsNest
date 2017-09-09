810ead32e5bb04afcf9f49a3e6c51bd379224b7d
Konsta <konsta09@gmail.com>
2017-09-09 19:15:43 +0800

    [2/3] Volume rocker cursor control
    Change-Id: I495cf22fefc949bae753ec6b6f6eeff650751307
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- src/com/aosip/owlsnest/buttons/VolumeCategory.java
+++ src/com/aosip/owlsnest/buttons/VolumeCategory.java
@@ -24,6 +24,7 @@ import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.content.res.Resources;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -38,6 +39,10 @@ public class VolumeCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
 
+    private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+    private ListPreference mVolumeKeyCursorControl;
+
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.OWLSNEST;
@@ -47,6 +52,20 @@ public class VolumeCategory extends SettingsPreferenceFragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.volume);
+
+	final ContentResolver resolver = getContentResolver();
+	final PreferenceScreen prefScreen = getPreferenceScreen();
+	final Resources res = getResources();
+
+	// volume key cursor control
+        mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
+        if (mVolumeKeyCursorControl != null) {
+		mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
+		int volumeRockerCursorControl = Settings.System.getInt(getContentResolver(),
+			Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
+		mVolumeKeyCursorControl.setValue(Integer.toString(volumeRockerCursorControl));
+		mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
+	}
     }
 
     @Override
@@ -55,6 +74,17 @@ public class VolumeCategory extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+	if (preference == mVolumeKeyCursorControl) {
+		String volumeKeyCursorControl = (String) newValue;
+		int volumeKeyCursorControlValue = Integer.parseInt(volumeKeyCursorControl);
+            	Settings.System.putInt(getActivity().getContentResolver(),
+                   	Settings.System.VOLUME_KEY_CURSOR_CONTROL, volumeKeyCursorControlValue);
+            	int volumeKeyCursorControlIndex = mVolumeKeyCursorControl
+                    	.findIndexOfValue(volumeKeyCursorControl);
+           	mVolumeKeyCursorControl
+                    	.setSummary(mVolumeKeyCursorControl.getEntries()[volumeKeyCursorControlIndex]);
+            	return true;
+	}
         return false;
     }
 
