82fde7dd01c7ced846cb020f148af3f23425451a
Adnan <adnan@cyngn.com>
2017-09-28 22:00:00 +0800

    HeadsUp: Stoplists [2/2]
    [POSP: @AgentFabulous]
    - Add indexing code and refactor
    
    aosip edit:
    -drop packagelist as we already have
    -dop headsup master switch as already have
    
    Change-Id: I00ddcda2f2e1889108e957ea830e4c127d455185
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/aosip/owlsnest/notification/HeadsUp.java
+++ src/com/aosip/owlsnest/notification/HeadsUp.java
@@ -16,7 +16,14 @@
 
 package com.aosip.owlsnest.notification;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -24,6 +31,16 @@ import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceViewHolder;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -31,9 +48,13 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
+import com.aosip.support.preference.PackageListAdapter.PackageItem;
+import com.aosip.support.preference.PackageListAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SearchIndexable
 public class HeadsUp extends SettingsPreferenceFragment implements
@@ -41,9 +62,15 @@ public class HeadsUp extends SettingsPreferenceFragment implements
 
     private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
     private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
-
+    private static final int DIALOG_STOPLIST_APPS = 0;
     private ListPreference mHeadsUpSnoozeTime;
     private ListPreference mHeadsUpTimeOut;
+    private Map<String, Package> mStoplistPackages;
+    private PackageListAdapter mPackageAdapter;
+    private PackageManager mPackageManager;
+    private PreferenceGroup mStoplistPrefList;
+    private Preference mAddStoplistPref;
+    private String mStoplistPackageList;
 
     @Override
     public int getMetricsCategory() {
@@ -54,8 +81,17 @@ public class HeadsUp extends SettingsPreferenceFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.headsup);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+        mPackageManager = getPackageManager();
+        mPackageAdapter = new PackageListAdapter(getActivity());
+        mStoplistPrefList = (PreferenceGroup) findPreference("stoplist_applications");
+        mStoplistPrefList.setOrderingAsAdded(false);
+        mStoplistPackages = new HashMap<String, Package>();
+        mAddStoplistPref = findPreference("add_stoplist_packages");
+        mAddStoplistPref.setOnPreferenceClickListener(this);
 
         Resources systemUiResources;
         try {
@@ -87,6 +123,7 @@ public class HeadsUp extends SettingsPreferenceFragment implements
     @Override
     public void onResume() {
         super.onResume();
+        refreshCustomApplicationPrefs();
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -122,6 +159,199 @@ public class HeadsUp extends SettingsPreferenceFragment implements
         String summary = getResources().getString(R.string.heads_up_time_out_summary,
                 value / 1000);
         mHeadsUpTimeOut.setSummary(summary);
+    }
+
+    /**
+     * Utility classes and supporting methods
+     */
+    @Override
+    public Dialog onCreateDialog(int id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final Dialog dialog;
+        switch (id) {
+            case DIALOG_STOPLIST_APPS:
+                final ListView list = new ListView(getActivity());
+                list.setAdapter(mPackageAdapter);
+
+                builder.setTitle(R.string.profile_choose_app);
+                builder.setView(list);
+                dialog = builder.create();
+
+                list.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // Add empty application definition, the user will be able to edit it later
+                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
+                        addCustomApplicationPref(info.packageName);
+                        dialog.cancel();
+                    }
+                });
+                break;
+            default:
+                dialog = null;
+        }
+        return dialog;
+    }
+
+    /**
+     * Application class
+     */
+    private static class Package {
+        public String name;
+        /**
+         * Stores all the application values in one call
+         * @param name
+         */
+        public Package(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append(name);
+            return builder.toString();
+        }
+
+        public static Package fromString(String value) {
+            if (TextUtils.isEmpty(value)) {
+                return null;
+            }
+
+            try {
+                Package item = new Package(value);
+                return item;
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+
+    };
+
+    private void refreshCustomApplicationPrefs() {
+        if (!parsePackageList()) {
+            return;
+        }
+
+        // Add the Application Preferences
+        if (mStoplistPrefList != null) {
+            mStoplistPrefList.removeAll();
+
+            for (Package pkg : mStoplistPackages.values()) {
+                try {
+                    Preference pref = createPreferenceFromInfo(pkg);
+                    mStoplistPrefList.addPreference(pref);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // Do nothing
+                }
+            }
+        }
+
+        // Keep these at the top
+        mAddStoplistPref.setOrder(0);
+        // Add 'add' options
+        mStoplistPrefList.addPreference(mAddStoplistPref);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (preference == mAddStoplistPref) {
+            showDialog(DIALOG_STOPLIST_APPS);
+        } else {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.dialog_delete_title)
+                    .setMessage(R.string.dialog_delete_message)
+                    .setIconAttribute(android.R.attr.alertDialogIcon)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            removeCustomApplicationPref(preference.getKey());
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, null);
+
+        builder.show();
+        }
+        return true;
+    }
+
+     private void addCustomApplicationPref(String packageName) {
+        Package pkg = mStoplistPackages.get(packageName);
+        if (pkg == null) {
+            pkg = new Package(packageName);
+            mStoplistPackages.put(packageName, pkg);
+            savePackageList(false);
+            refreshCustomApplicationPrefs();
+        }
+    }
+
+    private Preference createPreferenceFromInfo(Package pkg)
+            throws PackageManager.NameNotFoundException {
+        PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
+                PackageManager.GET_META_DATA);
+        Preference pref =
+                new Preference(getActivity());
+
+        pref.setKey(pkg.name);
+        pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
+        pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
+        pref.setPersistent(false);
+        pref.setOnPreferenceClickListener(this);
+        return pref;
+    }
+
+    private void removeCustomApplicationPref(String packageName) {
+        if (mStoplistPackages.remove(packageName) != null) {
+            savePackageList(false);
+            refreshCustomApplicationPrefs();
+        }
+    }
+
+    private boolean parsePackageList() {
+
+        final String stoplistString = Settings.System.getString(getContentResolver(),
+                Settings.System.HEADS_UP_STOPLIST_VALUES);
+
+        if (TextUtils.equals(mStoplistPackageList, stoplistString)) {
+            return false;
+        }
+            mStoplistPackageList = stoplistString;
+            mStoplistPackages.clear();
+
+        if (stoplistString != null) {
+            final String[] array = TextUtils.split(stoplistString, "\\|");
+            for (String item : array) {
+                if (TextUtils.isEmpty(item)) {
+                    continue;
+                }
+                Package pkg = Package.fromString(item);
+                if (pkg != null) {
+                    mStoplistPackages.put(pkg.name, pkg);
+                }
+            }
+        }
+
+        return true;
+    }
+
+     private void savePackageList(boolean preferencesUpdated) {
+        List<String> settings = new ArrayList<String>();
+        for (Package app : mStoplistPackages.values()) {
+            settings.add(app.toString());
+        }
+        final String value = TextUtils.join("|", settings);
+        if (preferencesUpdated) {
+                mStoplistPackageList = value;
+            }
+        Settings.System.putString(getContentResolver(),
+                Settings.System.HEADS_UP_STOPLIST_VALUES, value);
+    }
+
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        if (dialogId == DIALOG_STOPLIST_APPS) {
+            return MetricsProto.MetricsEvent.OWLSNEST;
+        }
+        return 0;
     }
 
     /**
