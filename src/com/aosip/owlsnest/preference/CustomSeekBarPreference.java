26b2ec8ba82a90541ee2e90122fa124f64a72cfd
ezio84 <brabus84@gmail.com>
2017-09-30 09:22:18 +0200

    Customseekbar: add method to set default values
    also be sure default values is not greater than max value
    
    Change-Id: Ie0e2c2d9a587186a42df7fb25fb4772ca9751f36

--- src/com/aosip/owlsnest/preference/CustomSeekBarPreference.java
+++ src/com/aosip/owlsnest/preference/CustomSeekBarPreference.java
@@ -54,6 +54,9 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 100);
         mMin = attrs.getAttributeIntValue(SETTINGS_NS, "min", 0);
         mDefaultValue = attrs.getAttributeIntValue(ANDROIDNS, "defaultValue", -1);
+        if (mDefaultValue > mMax) {
+            mDefaultValue = mMax;
+        }
         mUnits = getAttributeStringValue(attrs, SETTINGS_NS, "units", "");
         mDefaultText = getAttributeStringValue(attrs, SETTINGS_NS, "defaultText", "Def");
 
@@ -223,6 +226,16 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
             }
             persistInt(temp);
             mCurrentValue = temp;
+        }
+    }
+
+    public void setDefaultValue(int value) {
+        mDefaultValue = value;
+        if (mDefaultValue > mMax) {
+            mDefaultValue = mMax;
+        }
+        if (mCurrentValue == mDefaultValue) {
+            mStatusText.setText(mDefaultText);
         }
     }
 
