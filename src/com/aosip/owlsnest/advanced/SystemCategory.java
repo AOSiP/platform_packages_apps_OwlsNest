0f4572bda201127423a3e200f6cc99e38275e129
ezio84 <brabus84@gmail.com>
2017-10-03 11:05:01 +0530

    Wired/BT headset: make ringtone audio focus customizable [2/2]
    Change-Id: I604fb41e75bceef859cd8084539922218b29e1ba

--- src/com/aosip/owlsnest/advanced/SystemCategory.java
+++ src/com/aosip/owlsnest/advanced/SystemCategory.java
@@ -32,8 +32,10 @@ public class SystemCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+    private static final String RINGTONE_FOCUS_MODE = "ringtone_focus_mode";
 
     private ListPreference mLaunchPlayerHeadsetConnection;
+    private ListPreference mHeadsetRingtoneFocus;
 
     @Override
     public int getMetricsCategory() {
@@ -55,6 +57,12 @@ public class SystemCategory extends SettingsPreferenceFragment implements
         mLaunchPlayerHeadsetConnection.setSummary(mLaunchPlayerHeadsetConnection.getEntry());
         mLaunchPlayerHeadsetConnection.setOnPreferenceChangeListener(this);
 
+        mHeadsetRingtoneFocus = (ListPreference) findPreference(RINGTONE_FOCUS_MODE);
+        int mHeadsetRingtoneFocusValue = Settings.Global.getInt(resolver,
+                Settings.Global.RINGTONE_FOCUS_MODE, 0);
+        mHeadsetRingtoneFocus.setValue(Integer.toString(mHeadsetRingtoneFocusValue));
+        mHeadsetRingtoneFocus.setSummary(mHeadsetRingtoneFocus.getEntry());
+        mHeadsetRingtoneFocus.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -71,6 +79,14 @@ public class SystemCategory extends SettingsPreferenceFragment implements
                     mLaunchPlayerHeadsetConnection.getEntries()[index]);
             Settings.System.putIntForUser(resolver, Settings.System.HEADSET_CONNECT_PLAYER,
                     mLaunchPlayerHeadsetConnectionValue, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mHeadsetRingtoneFocus) {
+            int mHeadsetRingtoneFocusValue = Integer.valueOf((String) newValue);
+            int index = mHeadsetRingtoneFocus.findIndexOfValue((String) newValue);
+            mHeadsetRingtoneFocus.setSummary(
+                    mHeadsetRingtoneFocus.getEntries()[index]);
+            Settings.Global.putInt(resolver, Settings.Global.RINGTONE_FOCUS_MODE,
+                    mHeadsetRingtoneFocusValue);
             return true;
         }
         return false;
