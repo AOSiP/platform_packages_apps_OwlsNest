fc346405aee1281cb8afe64fbb2c324e2ac2c5a4
ezio84 <brabus84@gmail.com>
2018-01-30 12:51:23 +0100

    Smart notification sounds [2/2]
    Change-Id: I4577a59d4ca0b502037730dbce7c91b52db805c0

--- src/com/aosip/owlsnest/notification/NotificationCategory.java
+++ src/com/aosip/owlsnest/notification/NotificationCategory.java
@@ -32,6 +32,7 @@ public class NotificationCategory extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
     private static final String TAG = "NotificationCategory";
 
+    private ListPreference mAnnoyingNotification;
     private ListPreference mNoisyNotification;
     private ListPreference mTickerMode;
 
@@ -62,6 +63,13 @@ public class NotificationCategory extends SettingsPreferenceFragment implements
                 1, UserHandle.USER_CURRENT);
         mNoisyNotification.setValue(String.valueOf(mode));
         mNoisyNotification.setSummary(mNoisyNotification.getEntry());
+
+        mAnnoyingNotification = (ListPreference) findPreference("less_notification_sounds");
+        mAnnoyingNotification.setOnPreferenceChangeListener(this);
+        int threshold = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD,
+                0, UserHandle.USER_CURRENT);
+        mAnnoyingNotification.setValue(String.valueOf(threshold));
       }
 
     @Override
@@ -86,6 +94,11 @@ public class NotificationCategory extends SettingsPreferenceFragment implements
             int index = mNoisyNotification.findIndexOfValue((String) newValue);
             mNoisyNotification.setSummary(
                     mNoisyNotification.getEntries()[index]);
+            return true;
+        } else if (preference.equals(mAnnoyingNotification)) {
+            int mode = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, mode, UserHandle.USER_CURRENT);
             return true;
         }
        return false;
