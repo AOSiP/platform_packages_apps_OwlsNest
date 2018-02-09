a147f1e46eedebbbf6aecf54169df70ecaabcc26
Alex Cruz <du.alexcruz@gmail.com>
2018-02-08 22:16:20 -0500

    Set a text length on the carrier label
    Up til now this has been gone unnoticed. Without a text limit the user
    could very easily blow up the statusbar to the point that is not usable.
    
    Thanks to Randall for letting us know!
    
    Change-Id: I2813af9e12c6d29e020669e0626faec8422fedbe

--- src/com/aosip/owlsnest/statusbar/CarrierCategory.java
+++ src/com/aosip/owlsnest/statusbar/CarrierCategory.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.text.InputFilter;
 import android.text.Spannable;
 import android.text.TextUtils;
 import android.widget.EditText;
@@ -86,7 +87,9 @@ public class CarrierCategory extends SettingsPreferenceFragment implements
 
             // Set an EditText view to get user input
             final EditText input = new EditText(getActivity());
+            int maxLength = 10;
             input.setText(TextUtils.isEmpty(mCustomCarrierLabelText) ? "" : mCustomCarrierLabelText);
+            input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
             input.setSelection(input.getText().length());
             alert.setView(input);
             alert.setPositiveButton(getString(android.R.string.ok),
