9ae363360b44dbb90f2509e69ae052c792500b1c
Nico60 <ndhouailly@gmail.com>
2018-03-20 10:55:41 -0400

    Add interpolators to qs tiles animation [2/2]
    clean up some tabs for spaces
    
    Change-Id: I702fdb87ed821290b5a895381f7b987e3a36a58d

--- src/com/aosip/owlsnest/qssettings/ExpandedCategory.java
+++ src/com/aosip/owlsnest/qssettings/ExpandedCategory.java
@@ -34,11 +34,13 @@ public class ExpandedCategory extends SettingsPreferenceFragment implements
 
     private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
+    private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
     private static final String QS_PANEL_ALPHA = "qs_panel_alpha";
 
     private CustomSeekBarPreference mQsPanelAlpha;
     private ListPreference mTileAnimationStyle;
     private ListPreference mTileAnimationDuration;
+    private ListPreference mTileAnimationInterpolator;
 
     @Override
     public int getMetricsCategory() {
@@ -50,25 +52,30 @@ public class ExpandedCategory extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.expanded);
 
-	ContentResolver resolver = getActivity().getContentResolver();
+        ContentResolver resolver = getActivity().getContentResolver();
 
-	// QS animation
+        // QS animation
         mTileAnimationStyle = (ListPreference) findPreference(PREF_TILE_ANIM_STYLE);
-        int tileAnimationStyle = Settings.System.getIntForUser(resolver,
-		Settings.System.ANIM_TILE_STYLE, 0,
-                UserHandle.USER_CURRENT);
+        int tileAnimationStyle = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.ANIM_TILE_STYLE, 0, UserHandle.USER_CURRENT);
         mTileAnimationStyle.setValue(String.valueOf(tileAnimationStyle));
         updateTileAnimationStyleSummary(tileAnimationStyle);
-        updateAnimTileDuration(tileAnimationStyle);
+        updateAnimTileStyle(tileAnimationStyle);
         mTileAnimationStyle.setOnPreferenceChangeListener(this);
 
-	mTileAnimationDuration = (ListPreference) findPreference(PREF_TILE_ANIM_DURATION);
-        int tileAnimationDuration = Settings.System.getIntForUser(resolver,
-                Settings.System.ANIM_TILE_DURATION, 2000,
-                UserHandle.USER_CURRENT);
+        mTileAnimationDuration = (ListPreference) findPreference(PREF_TILE_ANIM_DURATION);
+        int tileAnimationDuration = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.ANIM_TILE_DURATION, 2000, UserHandle.USER_CURRENT);
         mTileAnimationDuration.setValue(String.valueOf(tileAnimationDuration));
         updateTileAnimationDurationSummary(tileAnimationDuration);
         mTileAnimationDuration.setOnPreferenceChangeListener(this);
+
+        mTileAnimationInterpolator = (ListPreference) findPreference(PREF_TILE_ANIM_INTERPOLATOR);
+        int tileAnimationInterpolator = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.ANIM_TILE_INTERPOLATOR, 0, UserHandle.USER_CURRENT);
+        mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
+        updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
+        mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
 
         mQsPanelAlpha = (CustomSeekBarPreference) findPreference(QS_PANEL_ALPHA);
         int qsPanelAlpha = Settings.System.getIntForUser(resolver,
@@ -85,31 +92,37 @@ public class ExpandedCategory extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
 	ContentResolver resolver = getActivity().getContentResolver();
 	if (preference == mTileAnimationStyle) {
-		int tileAnimationStyle = Integer.valueOf((String) newValue);
-		Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_STYLE,
-			tileAnimationStyle, UserHandle.USER_CURRENT);
-		updateTileAnimationStyleSummary(tileAnimationStyle);
-		updateAnimTileDuration(tileAnimationStyle);
-		return true;
-	} else if (preference == mTileAnimationDuration) {
-		int tileAnimationDuration = Integer.valueOf((String) newValue);
-		Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_DURATION,
-			tileAnimationDuration, UserHandle.USER_CURRENT);
-		updateTileAnimationDurationSummary(tileAnimationDuration);
-		return true;
+        int tileAnimationStyle = Integer.valueOf((String) newValue);
+        Settings.System.putIntForUser(getContentResolver(), Settings.System.ANIM_TILE_STYLE,
+            tileAnimationStyle, UserHandle.USER_CURRENT);
+        updateTileAnimationStyleSummary(tileAnimationStyle);
+        updateAnimTileStyle(tileAnimationStyle);
+        return true;
+    } else if (preference == mTileAnimationDuration) {
+        int tileAnimationDuration = Integer.valueOf((String) newValue);
+        Settings.System.putIntForUser(getContentResolver(), Settings.System.ANIM_TILE_DURATION,
+            tileAnimationDuration, UserHandle.USER_CURRENT);
+        updateTileAnimationDurationSummary(tileAnimationDuration);
+        return true;
     } else if (preference == mQsPanelAlpha) {
         int bgAlpha = (Integer) newValue;
         Settings.System.putIntForUser(getContentResolver(),
                 Settings.System.QS_PANEL_BG_ALPHA, bgAlpha,
                 UserHandle.USER_CURRENT);
         return true;
-	}
+    } else if (preference == mTileAnimationInterpolator) {
+        int tileAnimationInterpolator = Integer.valueOf((String) newValue);
+        Settings.System.putIntForUser(getContentResolver(), Settings.System.ANIM_TILE_INTERPOLATOR,
+                tileAnimationInterpolator, UserHandle.USER_CURRENT);
+        updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
+        return true;
+      }
     return false;
   }
 
     private void updateTileAnimationStyleSummary(int tileAnimationStyle) {
         String prefix = (String) mTileAnimationStyle.getEntries()[mTileAnimationStyle.findIndexOfValue(String
-		.valueOf(tileAnimationStyle))];
+                .valueOf(tileAnimationStyle))];
         mTileAnimationStyle.setSummary(getResources().getString(R.string.qs_set_animation_style, prefix));
     }
 
@@ -119,13 +132,21 @@ public class ExpandedCategory extends SettingsPreferenceFragment implements
         mTileAnimationDuration.setSummary(getResources().getString(R.string.qs_set_animation_duration, prefix));
     }
 
-    private void updateAnimTileDuration(int tileAnimationStyle) {
+    private void updateTileAnimationInterpolatorSummary(int tileAnimationInterpolator) {
+        String prefix = (String) mTileAnimationInterpolator.getEntries()[mTileAnimationInterpolator.findIndexOfValue(String
+                .valueOf(tileAnimationInterpolator))];
+        mTileAnimationInterpolator.setSummary(getResources().getString(R.string.qs_set_animation_interpolator, prefix));
+    }
+
+    private void updateAnimTileStyle(int tileAnimationStyle) {
         if (mTileAnimationDuration != null) {
             if (tileAnimationStyle == 0) {
                 mTileAnimationDuration.setSelectable(false);
+                mTileAnimationInterpolator.setSelectable(false);
             } else {
                 mTileAnimationDuration.setSelectable(true);
+                mTileAnimationInterpolator.setSelectable(true);
             }
-	}
+        }
     }
 }
