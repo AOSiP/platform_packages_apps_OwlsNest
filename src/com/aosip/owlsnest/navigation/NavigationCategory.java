f78902c5c7307d8333d7e629610639d561e52517
Akhil Narang <akhilnarang.1999@gmail.com>
2018-08-06 20:33:55 +0530

    OwlsNest: improve gestures/navbar switch
      * Disable gestures switch when navbar is enabled, and vice versa
      * When you disable navbar, it will toggle gestures, and vice versa
    
    Change-Id: I23f877f2aa57c9ca494b3ccad2491ce6b585e23b
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- src/com/aosip/owlsnest/navigation/NavigationCategory.java
+++ src/com/aosip/owlsnest/navigation/NavigationCategory.java
@@ -35,6 +35,7 @@ import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.aosip.owlsnest.preference.CustomSeekBarPreference;
+import com.aosip.owlsnest.preference.SystemSettingSwitchPreference;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.utils.du.ActionConstants;
 import com.android.internal.utils.du.Config;
@@ -44,6 +45,7 @@ import com.android.settings.R;
 
 public class NavigationCategory extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
     private static final String NAVBAR_VISIBILITY = "navbar_visibility";
+    private static final String KEY_GESTURE_NAVIGATION = "use_bottom_gesture_navigation";
     private static final String KEY_NAVBAR_MODE = "navbar_mode";
     private static final String KEY_DEFAULT_NAVBAR_SETTINGS = "default_settings";
     private static final String KEY_FLING_NAVBAR_SETTINGS = "fling_settings";
@@ -57,6 +59,7 @@ public class NavigationCategory extends SettingsPreferenceFragment implements Pr
     private static final String KEY_PULSE_SETTINGS = "pulse_settings";
 
     private SwitchPreference mNavbarVisibility;
+    private SystemSettingSwitchPreference mGestureNavigation;
     private ListPreference mNavbarMode;
     private PreferenceScreen mFlingSettings;
     private PreferenceCategory mNavInterface;
@@ -77,6 +80,7 @@ public class NavigationCategory extends SettingsPreferenceFragment implements Pr
         addPreferencesFromResource(R.xml.navigation);
 
         mNavInterface = (PreferenceCategory) findPreference(KEY_CATEGORY_NAVIGATION_INTERFACE);
+        mGestureNavigation = (SystemSettingSwitchPreference) findPreference(KEY_GESTURE_NAVIGATION);
         mNavGeneral = (PreferenceCategory) findPreference(KEY_CATEGORY_NAVIGATION_GENERAL);
         mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
         mNavbarMode = (ListPreference) findPreference(KEY_NAVBAR_MODE);
@@ -96,6 +100,11 @@ public class NavigationCategory extends SettingsPreferenceFragment implements Pr
 
         updateBarModeSettings(mode);
         mNavbarMode.setOnPreferenceChangeListener(this);
+
+        boolean gestures = Settings.System.getInt(getContentResolver(),
+                Settings.System.USE_BOTTOM_GESTURE_NAVIGATION, 0) == 1;
+        updateGestures(gestures);
+        mGestureNavigation.setOnPreferenceChangeListener(this);
 
         int size = Settings.Secure.getIntForUser(getContentResolver(),
                 Settings.Secure.NAVIGATION_BAR_HEIGHT, 100, UserHandle.USER_CURRENT);
@@ -161,6 +170,16 @@ public class NavigationCategory extends SettingsPreferenceFragment implements Pr
 
     private void updateBarVisibleAndUpdatePrefs(boolean showing) {
         mNavbarVisibility.setChecked(showing);
+        mGestureNavigation.setChecked(!showing);
+        mGestureNavigation.setEnabled(!showing);
+        mNavInterface.setEnabled(mNavbarVisibility.isChecked());
+        mNavGeneral.setEnabled(mNavbarVisibility.isChecked());
+    }
+
+    private void updateGestures(boolean showing) {
+        mGestureNavigation.setChecked(showing);
+        mNavbarVisibility.setChecked(!showing);
+        mNavbarVisibility.setEnabled(!showing);
         mNavInterface.setEnabled(mNavbarVisibility.isChecked());
         mNavGeneral.setEnabled(mNavbarVisibility.isChecked());
     }
@@ -181,6 +200,8 @@ public class NavigationCategory extends SettingsPreferenceFragment implements Pr
             boolean showing = ((Boolean)newValue);
             Settings.Secure.putInt(getContentResolver(), Settings.Secure.NAVIGATION_BAR_VISIBLE,
                     showing ? 1 : 0);
+            Settings.System.putInt(getContentResolver(), Settings.System.USE_BOTTOM_GESTURE_NAVIGATION,
+                    showing ? 0 : 1);
             updateBarVisibleAndUpdatePrefs(showing);
             mHandler.postDelayed(new Runnable() {
                 @Override
@@ -188,6 +209,14 @@ public class NavigationCategory extends SettingsPreferenceFragment implements Pr
                     mIsNavSwitchingMode = false;
                 }
             }, 1500);
+            return true;
+        } else if (preference == mGestureNavigation) {
+            boolean showing = ((Boolean)newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.USE_BOTTOM_GESTURE_NAVIGATION,
+                    showing ? 1 : 0);
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.NAVIGATION_BAR_VISIBLE,
+                    showing ? 0 : 1);
+            updateGestures(showing);
             return true;
         } else if (preference == mBarHeightPort) {
             int val = (Integer) newValue;
