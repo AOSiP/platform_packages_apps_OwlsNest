f61cf00501be4164b9fba9e6acd7fec3123c0c13
mydongistiny <jaysonedson@gmail.com>
2018-09-06 03:43:22 -0700

    Visualizer customizations
    Change-Id: If5064eb043e43e40ed8d9c8a70f886670ddf0500
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/aosip/owlsnest/lockscreen/LockVisualizerCategory.java
+++ src/com/aosip/owlsnest/lockscreen/LockVisualizerCategory.java
@@ -30,12 +30,17 @@ import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LockVisualizerCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
+
+    private static final String LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR = "lock_screen_visualizer_custom_color";
+
+    private ColorPickerPreference mVisualizerColor;
 
     @Override
     public int getMetricsCategory() {
@@ -46,6 +51,17 @@ public class LockVisualizerCategory extends SettingsPreferenceFragment implement
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lock_visualizer);
+	    ContentResolver resolver = getActivity().getContentResolver();
+
+        // Visualizer custom color
+        mVisualizerColor = (ColorPickerPreference) findPreference(LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR);
+        int visColor = Settings.System.getInt(resolver,
+                Settings.System.LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR, 0xff1976D2);
+        String visColorHex = String.format("#%08x", (0xff1976D2 & visColor));
+        mVisualizerColor.setSummary(visColorHex);
+        mVisualizerColor.setNewPreviewColor(visColor);
+        mVisualizerColor.setAlphaSliderEnabled(true);
+        mVisualizerColor.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -54,6 +70,16 @@ public class LockVisualizerCategory extends SettingsPreferenceFragment implement
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+		ContentResolver resolver = getActivity().getContentResolver();
+	    if (preference == mVisualizerColor) {
+			String hex = ColorPickerPreference.convertToARGB(
+			Integer.valueOf(String.valueOf(newValue)));
+			int intHex = ColorPickerPreference.convertToColorInt(hex);
+			Settings.System.putInt(resolver,
+			Settings.System.LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR, intHex);
+			preference.setSummary(hex);
+			return true;
+        }
         return false;
     }
 
