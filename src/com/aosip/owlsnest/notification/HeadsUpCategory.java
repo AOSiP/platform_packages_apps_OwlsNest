32bd64af856b7ce7fb9d65893f2658d2edebd253
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2018-10-28 09:20:43 -0400

    OwlsNest: Add Nest to SearchIndex (2/2)
    -remove some unused imports
    -make MetricsEvent uniform
    
    Change-Id: I3eaa4003c6151831c33a025bac1ee37da45fbd7d
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/aosip/owlsnest/notification/HeadsUpCategory.java
+++ src/com/aosip/owlsnest/notification/HeadsUpCategory.java
@@ -17,21 +17,28 @@
 package com.aosip.owlsnest.notification;
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
-import android.provider.Settings;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HeadsUpCategory extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, Indexable {
+
     private static final String TAG = "HeadsUpCategory";
 
     private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
@@ -119,5 +126,26 @@ public class HeadsUpCategory extends SettingsPreferenceFragment implements
             mHeadsUpSnoozeTime.setSummary(summary);
         }
     }
+
+    /**
+     * For Search.
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                 @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                     final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.headsup;
+                    result.add(sir);
+                    return result;
+                }
+                 @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
 
