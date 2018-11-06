12347685fe8af11dc90ab3dc162667308f2bdee1
SpiritCroc <spiritcroc@gmail.com>
2018-11-06 11:30:18 +0530

    [SQUASHED] Status bar notification ticker [2/2]
    Configurable status bar ticker duration [1/2]
    
    Change-Id: I3d399151a8ddfe183ca94ddf46736d350b0cb098
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/aosip/owlsnest/statusbar/StatusbarHolder.java
+++ src/com/aosip/owlsnest/statusbar/StatusbarHolder.java
@@ -85,7 +85,8 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
             frags[1] = new CarrierCategory();
             frags[2] = new ClockDateSettings();
             frags[3] = new IconsCategory();
-            frags[4] = new TrafficCategory();
+            frags[4] = new TickerCategory();
+            frags[5] = new TrafficCategory();
         }
 
         @Override
@@ -111,6 +112,7 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
                     getString(R.string.carrier_category),
                     getString(R.string.clock_category),
                     getString(R.string.icon_category),
+                    getString(R.string.ticker_category),
                     getString(R.string.traffic_category)};
         return titleString;
     }
