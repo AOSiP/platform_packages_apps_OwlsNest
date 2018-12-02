68b30259284ca87347620de03dbb5a09be361820
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2018-12-01 21:13:06 -0500

    OwlsNest: Clean up some code
    -remove unused battery page
    -remove setHasOptionsMenu as not needed
    -remove unneeded imports from holder pages
    -remove some unused lines
    -remove a bit of unused code from slider
    
    Change-Id: Ia8b580e76cf604e4c2cefc36bd00b2a88c54d5fc
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/aosip/owlsnest/quicksettings/QuickSettingsHolder.java
+++ src/com/aosip/owlsnest/quicksettings/QuickSettingsHolder.java
@@ -31,7 +31,6 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
-import com.aosip.owlsnest.quicksettings.ExpandedCategory;
 import com.aosip.owlsnest.PagerSlidingTabStrip;
 
 public class QuickSettingsHolder extends SettingsPreferenceFragment {
@@ -45,15 +44,12 @@ public class QuickSettingsHolder extends SettingsPreferenceFragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mContainer = container;
-
         View view = inflater.inflate(R.layout.preference_ui, container, false);
         mViewPager = (ViewPager) view.findViewById(R.id.pager);
         mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
         StatusBarAdapter StatusBarAdapter = new StatusBarAdapter(getFragmentManager());
         mViewPager.setAdapter(StatusBarAdapter);
         mTabs.setViewPager(mViewPager);
-
-        setHasOptionsMenu(true);
         return view;
     }
 
@@ -109,5 +105,3 @@ public class QuickSettingsHolder extends SettingsPreferenceFragment {
         return titleString;
     }
 }
-
-
