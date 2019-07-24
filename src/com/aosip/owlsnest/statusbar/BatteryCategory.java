4a84f841898819c3827b1c5d4c9e20153dd969ac
AnkitGourav <Ankitgourav035@gmail.com>
2019-07-24 21:12:47 +0530

    Battery Estimates options [2/2]
    - Adapted for our settings
    - Don't add SystemUI Restart Dialog
    
    Co-authored-by: Stallix <team.evolutionx.rom@gmail.com>
    Change-Id: I051286d242b99ff2d2e62e630e77299a99c36942
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/aosip/owlsnest/statusbar/BatteryCategory.java
+++ src/com/aosip/owlsnest/statusbar/BatteryCategory.java
@@ -19,18 +19,24 @@ package com.aosip.owlsnest.statusbar;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.statusbar.IStatusBarService;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class BatteryCategory extends SettingsPreferenceFragment implements
@@ -38,11 +44,15 @@ public class BatteryCategory extends SettingsPreferenceFragment implements
 
     private static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String BATTERY_ESTIMATE_POSITION_TYPE = "battery_estimate_position";
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 3;
     private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 4;
 
     private ListPreference mStatusBarBatteryShowPercent;
     private ListPreference mStatusBarBattery;
+    private ListPreference mEstimatePositionType;
+
+    private IStatusBarService mStatusBarService;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,6 +80,14 @@ public class BatteryCategory extends SettingsPreferenceFragment implements
         mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
         enableStatusBarBatteryDependents(batteryStyle);
         mStatusBarBattery.setOnPreferenceChangeListener(this);
+
+        // battery estimate position
+        mEstimatePositionType = (ListPreference) findPreference(BATTERY_ESTIMATE_POSITION_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.BATTERY_ESTIMATE_POSITION, 0);
+        mEstimatePositionType.setValue(String.valueOf(type));
+        mEstimatePositionType.setSummary(mEstimatePositionType.getEntry());
+        mEstimatePositionType.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -90,6 +108,21 @@ public class BatteryCategory extends SettingsPreferenceFragment implements
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE, batteryStyle);
             mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
             enableStatusBarBatteryDependents(batteryStyle);
+            return true;
+        } else if (preference == mEstimatePositionType) {
+            int type = Integer.valueOf((String) objValue);
+            int index = mEstimatePositionType.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.BATTERY_ESTIMATE_POSITION, type);
+            mEstimatePositionType.setSummary(mEstimatePositionType.getEntries()[index]);
+            IStatusBarService statusBarService = IStatusBarService.Stub.asInterface(ServiceManager.checkService(Context.STATUS_BAR_SERVICE));
+            if (statusBarService != null) {
+                try {
+                    statusBarService.restartUI();
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
+            }
             return true;
         }
         return false;
