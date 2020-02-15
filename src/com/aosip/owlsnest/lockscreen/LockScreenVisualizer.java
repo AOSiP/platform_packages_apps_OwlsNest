2bc904c56d87512259b09114ff6efe3cccdaedb1
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-02-15 14:11:07 -0500

    OwlsNest: Annual Cleanup
    - redo statusbar section
    - remove leftover stuff
    - update all copyright dates
    
    Change-Id: Ic81265441d89c12ad4665ca2956a53bef145a0cb

--- src/com/aosip/owlsnest/lockscreen/LockScreenVisualizer.java
+++ src/com/aosip/owlsnest/lockscreen/LockScreenVisualizer.java
@@ -1,11 +1,11 @@
- /*
- * Copyright (C) 2019 crDroid Android Project
+/*
+ *  Copyright (C) 2015-2020 Android Open Source Illusion Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.aosip.owlsnest.lockscreen;
 
 import android.app.Activity;
@@ -21,8 +22,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
@@ -30,14 +31,21 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
-import com.aosip.owlsnest.utils.Utils;
-import com.android.settings.SettingsPreferenceFragment;
-
 import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.aosip.owlsnest.utils.Utils;
 import com.aosip.support.preference.CustomSeekBarPreference;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable
 public class LockScreenVisualizer extends SettingsPreferenceFragment
-            implements Preference.OnPreferenceChangeListener  {
+            implements Preference.OnPreferenceChangeListener, Indexable {
 
     public static final String TAG = "LockScreenVisualizer";
 
@@ -118,4 +126,26 @@ public class LockScreenVisualizer extends SettingsPreferenceFragment
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.OWLSNEST;
     }
+
+    /**
+     * For Search.
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider() {
+            @Override
+            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context, boolean enabled) {
+                ArrayList<SearchIndexableResource> result =
+                    new ArrayList<SearchIndexableResource>();
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.lockscreen_visualizer;
+                    result.add(sir);
+                    return result;
+            }
+
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                List<String> keys = super.getNonIndexableKeys(context);
+                return keys;
+            }
+        };
 }
--- src/com/aosip/owlsnest/statusbar/CarrierLabel.java
+++ src/com/aosip/owlsnest/statusbar/CarrierLabel.java
@@ -173,24 +173,25 @@ public class CarrierLabel extends SettingsPreferenceFragment
         return MetricsProto.MetricsEvent.OWLSNEST;
     }
 
+    /**
+     * For Search.
+     */
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                                                                            boolean enabled) {
-                    ArrayList<SearchIndexableResource> result =
-                            new ArrayList<SearchIndexableResource>();
-
+        new BaseSearchIndexProvider() {
+            @Override
+            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context, boolean enabled) {
+                ArrayList<SearchIndexableResource> result =
+                    new ArrayList<SearchIndexableResource>();
                     SearchIndexableResource sir = new SearchIndexableResource(context);
                     sir.xmlResId = R.xml.carrier_label;
                     result.add(sir);
                     return result;
-                }
+            }
 
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
-                }
-            };
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                List<String> keys = super.getNonIndexableKeys(context);
+                return keys;
+            }
+        };
 }
