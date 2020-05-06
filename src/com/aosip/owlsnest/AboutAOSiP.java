e44f03c0e03d0f7837247d397fd7c889a81715f0
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-05-05 21:34:32 -0400

    OwlsNest: Add a simple About AOSiP page
    -display a little info about the team
    -maintainer string is overlayable in device tree
    
    Change-Id: Icdba63a636b076d0b7709feaa608512997073d48

---
+++ src/com/aosip/owlsnest/AboutAOSiP.java
@@ -0,0 +1,34 @@
+package com.aosip.owlsnest;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class AboutAOSiP extends SettingsPreferenceFragment {
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.OWLSNEST;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.about);
+    }
+
+    @Override
+    public void setDivider(Drawable divider) {
+        super.setDivider(new ColorDrawable(Color.TRANSPARENT));
+    }
+
+    @Override
+    public void setDividerHeight(int height) {
+        super.setDividerHeight(0);
+    }
+}
