f9761bac9121eb696e0d62c4bc5b0c845f95900a
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-05-05 21:34:32 -0400

    OwlsNest: Add a simple About AOSiP page
    -display a little info about the team
    -maintainer string is overlayable in device tree
    
    Change-Id: Icdba63a636b076d0b7709feaa608512997073d48

---
+++ src/com/aosip/owlsnest/AboutAOSiP.java
@@ -0,0 +1,22 @@
+package com.aosip.owlsnest;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class AboutAOSiP extends SettingsPreferenceFragment {
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.OWLSNEST;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.about);
+    }
+}
