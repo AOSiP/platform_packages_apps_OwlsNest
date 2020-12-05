dcdf7d8e9237eaa9070d007b88bf45ee29e7800b
calebcabob <calphonic@gmail.com>
2020-12-05 20:32:25 +0530

    OwlsNest: Bring in °F/°C toggle [2/2]
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I66022584d4b2aea33ce3e7a9cd7921869a3a7b72
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- src/com/aosip/owlsnest/fragments/lockscreen/LockscreenGeneral.java
+++ src/com/aosip/owlsnest/fragments/lockscreen/LockscreenGeneral.java
@@ -16,6 +16,7 @@
 
 package com.aosip.owlsnest.fragments.lockscreen;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
@@ -23,6 +24,8 @@ import android.provider.Settings;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
@@ -30,7 +33,7 @@ import com.android.settingslib.search.SearchIndexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-import com.android.internal.logging.nano.MetricsProto;
+import com.aosip.support.preference.SecureSettingListPreference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,9 +46,29 @@ public class LockscreenGeneral extends SettingsPreferenceFragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lockscreen_general);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        int unitMode = Settings.System.getIntForUser(resolver,
+                Settings.System.LOCKSCREEN_BATTERY_INFO_TEMP_UNIT, 0, UserHandle.USER_CURRENT);
+        mBatteryTempUnit = (SystemSettingListPreference) findPreference(
+                "lockscreen_charge_temp_unit");
+        mBatteryTempUnit.setValue(String.valueOf(unitMode));
+        mBatteryTempUnit.setSummary(mBatteryTempUnit.getEntry());
+        mBatteryTempUnit.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBatteryTempUnit) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.LOCKSCREEN_BATTERY_INFO_TEMP_UNIT, value,
+                    UserHandle.USER_CURRENT);
+            int index = mBatteryTempUnit.findIndexOfValue((String) newValue);
+            mBatteryTempUnit.setSummary(
+            mBatteryTempUnit.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 
