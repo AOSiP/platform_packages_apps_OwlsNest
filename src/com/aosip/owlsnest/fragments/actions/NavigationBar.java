6d06a2aec9d8be8502781d6c18e2fc7640ebac14
bigrushdog <randall.rushing@gmail.com>
2020-12-09 14:43:07 -0500

    Add navigation bar visibility control [2/2]
    Change-Id: I5eb9f3e452102c62555fb117256abf464ee5c2b5

--- src/com/aosip/owlsnest/fragments/actions/NavigationBar.java
+++ src/com/aosip/owlsnest/fragments/actions/NavigationBar.java
@@ -16,8 +16,10 @@
 
 package com.aosip.owlsnest.fragments.actions;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import androidx.preference.*;
@@ -31,6 +33,7 @@ import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.util.aosip.AOSiPUtils;
+import com.android.internal.util.aosip.ActionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,10 +45,15 @@ public class NavigationBar extends SettingsPreferenceFragment implements
     private static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
     private static final String LAYOUT_SETTINGS = "navbar_layout_views";
     private static final String NAVIGATION_BAR_INVERSE = "navbar_inverse_layout";
+    private static final String NAVBAR_VISIBILITY = "navbar_visibility";
 
     private SwitchPreference mPixelNavAnimation;
     private Preference mLayoutSettings;
     private SwitchPreference mSwapNavButtons;
+    private SwitchPreference mNavbarVisibility;
+
+    private boolean mIsNavSwitchingMode = false;
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,14 +65,47 @@ public class NavigationBar extends SettingsPreferenceFragment implements
         mPixelNavAnimation = (SwitchPreference) findPreference(PIXEL_NAV_ANIMATION);
         mLayoutSettings = (Preference) findPreference(LAYOUT_SETTINGS);
         mSwapNavButtons = (SwitchPreference) findPreference(NAVIGATION_BAR_INVERSE);
+        mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
 
         if (!AOSiPUtils.isThemeEnabled("com.android.internal.systemui.navbar.threebutton")) {
             prefScreen.removePreference(mLayoutSettings);
         }
+
+        boolean defaultToNavigationBar = ActionUtils.deviceSupportNavigationBar(getActivity());
+        boolean showing = Settings.System.getInt(getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR,
+                defaultToNavigationBar ? 1 : 0) != 0;
+        updateBarVisibleAndUpdatePrefs(showing);
+
+        mNavbarVisibility.setOnPreferenceChangeListener(this);
+
+        mHandler = new Handler();
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mNavbarVisibility)) {
+            if (mIsNavSwitchingMode) {
+                return false;
+            }
+            mIsNavSwitchingMode = true;
+            boolean showing = ((Boolean)newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.FORCE_SHOW_NAVBAR,
+                    showing ? 1 : 0);
+            updateBarVisibleAndUpdatePrefs(showing);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsNavSwitchingMode = false;
+                }
+            }, 1500);
+            return true;
+        }
         return false;
+    }
+
+    private void updateBarVisibleAndUpdatePrefs(boolean showing) {
+        mNavbarVisibility.setChecked(showing);
     }
 
     @Override
